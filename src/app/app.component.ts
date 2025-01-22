28267882ffaf78f644854c03ef84438f1136d3d3
Carlo Amenta <carlo.amenta@docebo.com>
2025-01-22 20:00:37 +0100

    try login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pegaso-booking';
+export class AppComponent implements OnInit {
+    title = 'pegaso-booking';
+
+    ngOnInit(): void {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, '', '')
+            .then((userCredential) => console.log(userCredential.user))
+            .catch((error) => console.error(error));
+    }
 }
