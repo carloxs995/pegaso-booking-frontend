46804eeb811cede6c11e117ee431e245a79c33b6
Carlo Amenta <carlo.amenta@docebo.com>
2025-01-26 14:03:05 +0100

    improved card

--- src/app/components/rooms/room-card/room-card.component.ts
+++ src/app/components/rooms/room-card/room-card.component.ts
@@ -1,14 +1,17 @@
-import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, inject, Input, ViewEncapsulation } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IRoomDetails } from '../../../models/room.models';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-room-card',
     encapsulation: ViewEncapsulation.None,
     imports: [
         MatCardModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTooltipModule
     ],
     templateUrl: './room-card.component.html',
     styleUrl: './room-card.component.scss'
@@ -17,4 +20,10 @@ export class RoomCardComponent {
     @HostBinding('className') className = 'room-card';
 
     @Input() item!: IRoomDetails;
+
+    private readonly _router = inject(Router);
+
+    onNavigateToDetails() {
+        this._router.navigate([`/rooms/${this.item.id}`]);
+    }
 }
