7aa7cfc690c542c1abff5834e1f7ee77a8e41e0b
Carlo Amenta <carlo.amenta@docebo.com>
2025-01-27 18:56:22 +0100

    fix

--- src/app/components/core/navbar/navigation-menu/navigation-menu.component.ts
+++ src/app/components/core/navbar/navigation-menu/navigation-menu.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../../services/authentication.service';
 
 @Component({
     selector: 'navigation-menu',
@@ -17,16 +18,19 @@ import { Router } from '@angular/router';
             <mat-icon>home</mat-icon>
             Home
         </button>
-        <button mat-menu-item (click)="navigateTo('/bookings')">
-        <mat-icon>list</mat-icon>
-                    Lista Prenotazioni
-        </button>
+        <ng-container *ngIf="!!authenticationService.currentUserData$.getValue()">
+            <button mat-menu-item (click)="navigateTo('/bookings')">
+            <mat-icon>list</mat-icon>
+                        Lista Prenotazioni
+            </button>
+        </ng-container>
     </mat-menu>
   `,
 })
 export class NavigationMenuComponent {
 
     private readonly _router: Router = inject(Router);
+    public authenticationService: AuthenticationService = inject(AuthenticationService);
 
     navigateTo(route: string): void {
         this._router.navigate([route]);
